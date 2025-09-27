5c330298b0f982ab74f9c9ac9df7b6075d4bb558
Feyza Narinc <feynarinc@gmail>
2025-09-27 15:10:34 +0100

    Added Helper class with getGreeting method and used it in Main

---
+++ src/day01_commit_push_pull/Helper.java
@@ -0,0 +1,7 @@
+package day01_commit_push_pull;
+
+public class Helper {
+    public static String getGreeting(){
+        return "Hello from Helper!";
+    }
+}
--- src/day01_commit_push_pull/Main.java
+++ src/day01_commit_push_pull/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
         Table.multiplicationTable(5);
         Table.multiplicationTable(3);
+
+        System.out.println(Helper.getGreeting());
     }
 }
 
